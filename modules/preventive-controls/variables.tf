26526ac47eb9b1abfdaa1c7b14567e1aa498ebae
Paul Shonowo <paulshonowo.dev@gmail.com>
2026-08-06 16:41:35 +0000

    fix: add variables.tf to preventive-controls module

---
+++ modules/preventive-controls/variables.tf
@@ -0,0 +1,9 @@
+variable "org_id" {
+  type        = string
+  description = "The GCP Organization ID"
+}
+
+variable "billing_account" {
+  type        = string
+  description = "The GCP Billing Account ID"
+}
