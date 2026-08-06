373c8fc20f7af5d81d185ddad9b7dc4f83e2db2e
Paul Shonowo <paulshonowo.dev@gmail.com>
2026-08-06 16:49:52 +0000

    fix: declare allowed_regions and enable_strict_external_ip_block variables

--- modules/preventive-controls/variables.tf
+++ modules/preventive-controls/variables.tf
@@ -7,3 +7,15 @@ variable "billing_account" {
   type        = string
   description = "The GCP Billing Account ID"
 }
+
+variable "allowed_regions" {
+  type        = list(string)
+  description = "Allowed GCP regions for organization policy restriction"
+  default     = ["in:eu-locations", "in:us-locations"]
+}
+
+variable "enable_strict_external_ip_block" {
+  type        = bool
+  description = "Whether to block external IP creation on VM instances"
+  default     = true
+}
